0c21cc5de83cab31ef70a05b3ec9ac74ceb3bc0b
TerryFei <v-tiafe@microsoft.com>
2018-06-11 15:55:55 +0800

    Push project SharePoint 2010: Creating Trusted Login Providers (SAML Sign-in)

---
+++ Microsoft Office Developer Documentation Team/SharePoint-2010-Creating-Trusted-Login-Providers-3919/[C#]-SharePoint 2010 Creating Trusted Login Providers (SAML Sign-in)/C#/RegisterSTS/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.IdentityModel.Claims;
+using Microsoft.SharePoint.Administration.Claims;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+
+namespace RegisterSTS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<SPTrustedClaimTypeInformation> claimMapping = new List<SPTrustedClaimTypeInformation>();
+            List<string> strClaimMapping = new List<string>();
+
+            SPTrustedClaimTypeInformation idClaim = new SPTrustedClaimTypeInformation("EmailAddress", "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress", "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress");
+            SPTrustedClaimTypeInformation titleClaim = new SPTrustedClaimTypeInformation("Title", "http://schemas.wingtip.com/sharepoint/2009/08/claims/title", "http://schemas.wingtip.com/sharepoint/2009/08/claims/title");
+
+            titleClaim.AcceptOnlyKnownClaimValues = true;
+
+            idClaim.AddKnownClaimValue("user1@wingtip.com");
+            idClaim.AddKnownClaimValue("user2@wingtip.com");
+            idClaim.AddKnownClaimValue("user3@wingtip.com");
+
+            titleClaim.AddKnownClaimValue("Engineer");
+            titleClaim.AddKnownClaimValue("Manager");
+            titleClaim.AddKnownClaimValue("CEO");
+
+            //creating the string[] for all claims, this is required for the construction of SPTrustedLoginProvider
+            strClaimMapping.Add(idClaim.InputClaimType);
+            strClaimMapping.Add(titleClaim.InputClaimType);
+
+
+            X509Certificate2 ImportTrustCertificate = new X509Certificate2(@"C:\StudentFiles\LabFiles\Module_6\Resources\STSTestCertPub.cer");
+
+            claimMapping.Add(idClaim);
+            claimMapping.Add(titleClaim);
+
+            SPSecurityTokenServiceManager manager = SPSecurityTokenServiceManager.Local;
+            SPTrustedLoginProvider provider = new SPTrustedLoginProvider(
+                                                                        manager,
+                                                                        "WingtipSTS",
+                                                                        "WingtipSTS",
+                                                                        new Uri("http://localhost:48924/WingtipSTS/default.aspx"),
+                                                                        "https://spdev0223/_trust/",
+                                                                        strClaimMapping.ToArray(),
+                                                                        idClaim);
+
+
+
+            foreach (SPTrustedClaimTypeInformation claimTypeInfo in claimMapping)
+            {
+                if (claimTypeInfo.InputClaimType == provider.IdentityClaimTypeInformation.InputClaimType)
+                {
+                    continue;
+                }
+                provider.AddClaimTypeInformation(claimTypeInfo);
+            }
+
+            if (ImportTrustCertificate != null)
+            {
+                provider.SigningCertificate = ImportTrustCertificate;
+            }
+
+
+            //provider.ClaimProviderName = "ContosoCRMClaimProvider";
+
+            provider.UseWReplyParameter = true;
+
+            manager.TrustedLoginProviders.Add(provider);
+            manager.Update();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            SPSecurityTokenServiceManager manager = SPSecurityTokenServiceManager.Local;
+            SPTrustedLoginProviderCollection providers = manager.TrustedLoginProviders;
+            
+            foreach(SPTrustedLoginProvider provider in providers)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add(provider.Name);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SPSecurityTokenServiceManager manager = SPSecurityTokenServiceManager.Local;
+
+            string providerName = (string)listBox1.SelectedItem;
+
+            SPTrustedLoginProvider provider = manager.TrustedLoginProviders[providerName];
+
+            manager.TrustedLoginProviders.Remove(provider.Id);
+            manager.Update();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SPSecurityTokenServiceManager manager = SPSecurityTokenServiceManager.Local;
+
+            string providerName = (string)listBox1.SelectedItem;
+
+            SPTrustedLoginProvider provider = manager.TrustedLoginProviders[providerName];
+            provider.UseWReplyParameter = true;
+            provider.ProviderRealm = "https://spdev0223/_trust/";
+            provider.ProviderUri = new Uri("http://localhost:48924/WingtipSTS/default.aspx");
+
+
+            provider.Update();
+
+            manager.Update();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SPSecurityTokenServiceManager manager = SPSecurityTokenServiceManager.Local;
+
+            string providerName = (string)listBox1.SelectedItem;
+
+            SPTrustedLoginProvider provider = manager.TrustedLoginProviders[providerName];
+            manager.TrustedLoginProviders.Remove(provider.Id);
+            manager.Update();
+        }
+    }
+}
---
+++ Microsoft Office Developer Documentation Team/SharePoint-2010-Creating-Trusted-Login-Providers-3919/[C#]-SharePoint 2010 Creating Trusted Login Providers (SAML Sign-in)/C#/WingtipSTS/App_Code/UserInfo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.IdentityModel.Claims;
+
+public class WingtipClaimTypes
+{
+    // System.IdentityModel.Claims.ClaimTypes.Email;
+    public static string EmailAddress = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress";
+    public static string Title = "http://schemas.wingtip.com/sharepoint/2009/08/claims/title";
+}
+
+public class UserInfo
+{
+    //Email address is used as the UserID.
+    //Every user has two claims: title and email address. SharePoint will pick up the email and treat it as identity.
+    private static string[] userDB = 
+       {
+        "user1@wingtip.com:Title:Engineer", 
+        "user1@wingtip.com:Email:user1@wingtip.com",
+        "user2@wingtip.com:Title:Manager",
+        "user2@wingtip.com:Email:user2@wingtip.com",
+        "user3@wingtip.com:Title:CEO",
+        "user3@wingtip.com:Email:user3@wingtip.com",
+       };
+
+    //Manually construct a list of user. In a real production environment
+    //You should look up a directory serivce or database to retrieve the user information.
+    public static List<string> GetAllUsers()
+    {
+        List<string> allUsers = new List<string>();
+        //adding forms-based users.
+        allUsers.Add("user1@wingtip.com");
+        allUsers.Add("user2@wingtip.com");
+        allUsers.Add("user3@wingtip.com");
+        return allUsers;
+    }
+
+    public static bool AuthenticateUser(string username, string password)
+    {
+        //Your authentication logic goes here.
+        return true;
+    }
+
+    /// <summary>
+    /// A real implementation should look up a directory service or database. 
+    /// to retrieve user's claim. The code below is used only for demostration purpose.
+    /// </summary>
+    /// <param name="username"></param>
+    /// <returns></returns>
+    public static List<Claim> GetClaimsForUser(string username)
+    {
+        List<Claim> userClaims = new List<Claim>();
+        foreach (string userInfo in userDB)
+        {
+            string[] claims = userInfo.Split(new string[] { ":" }, StringSplitOptions.RemoveEmptyEntries);
+            if (username == claims[0])
+            {
+                userClaims.Add(new Claim(GetClaimTypeForRole(claims[1]), claims[2], Microsoft.IdentityModel.Claims.ClaimValueTypes.String));
+            }
+        }
+
+        return userClaims;
+    }
+
+    public static string GetClaimTypeForRole(string roleName)
+    {
+        if (roleName.Equals("Title", StringComparison.OrdinalIgnoreCase))
+            return WingtipClaimTypes.Title;
+        else if (roleName.Equals("Email", StringComparison.OrdinalIgnoreCase))
+            return WingtipClaimTypes.EmailAddress;
+        else
+            throw new Exception("Claim Type not found!");
+    }
+
+}
