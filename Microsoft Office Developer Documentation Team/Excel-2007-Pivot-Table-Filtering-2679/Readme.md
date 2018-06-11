b44ab77e3781391171b93b215b19cde69152b377
TerryFei <v-tiafe@microsoft.com>
2018-06-11 15:44:48 +0800

    Push project Excel 2007: Pivot Table Filtering

---
+++ Microsoft Office Developer Documentation Team/Excel-2007-Pivot-Table-Filtering-2679/Readme.md
@@ -0,0 +1,17 @@
+# Excel 2007: Pivot Table Filtering
+## Requires
+* Visual Studio 2005
+## License
+* Apache License, Version 2.0
+## Technologies
+* Office
+* Excel 2007
+## Topics
+* pivot table filtering
+## IsPublished
+* True
+## ModifiedDate
+* 2011-03-30 01:12:32
+## Description
+
+<div class="wikidoc"><span style="font-size:small">This is the code sample that goes with the recently published MSDN technical article: Filtering Data Programmatically in Excel 2007 by Using PivotTables.</span></div>
---
+++ Microsoft Office Developer Documentation Team/Excel-2007-Pivot-Table-Filtering-2679/[C#]-Excel 2007 Pivot Table Filtering/C#/LaunchXL/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LaunchXL
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Microsoft.Office.Interop.Excel.Application m_xlApp = new Microsoft.Office.Interop.Excel.Application();
+			Microsoft.Office.Interop.Excel.Workbook xlWB; 
+			Microsoft.Office.Interop.Excel.Worksheet xlWS;
+			Microsoft.Office.Interop.Excel.Range xlRG;
+			Microsoft.Office.Interop.Excel.PivotCache xlPC;
+			object miss = Type.Missing;
+
+			m_xlApp.Visible = true;
+			const string olapConnectionString = "OLEDB;Provider=MSOLAP.2;Integrated Security=SSPI;Persist Security Info=True;Data Source=xlextdat8;Client Cache Size=25;Auto Synch Period=10000;Initial Catalog=FoodMart 2000";
+			const string cubeName = "Sales";
+
+			xlWB = m_xlApp.Workbooks.Add(miss);
+			xlWS = (Microsoft.Office.Interop.Excel.Worksheet)xlWB.Worksheets[1];
+			xlRG = xlWS.get_Range("A1", miss);
+
+			// create PT connected to OLAP data source
+			xlPC = (Microsoft.Office.Interop.Excel.PivotCache)((Microsoft.Office.Interop.Excel.PivotCaches)xlWB.PivotCaches()).Add(Microsoft.Office.Interop.Excel.XlPivotTableSourceType.xlExternal, miss, Microsoft.Office.Interop.Excel.XlPivotTableVersionList.xlPivotTableVersionCurrent);
+			xlPC.Connection = olapConnectionString;
+			xlPC.CommandType = Microsoft.Office.Interop.Excel.XlCmdType.xlCmdCube;
+			xlPC.CommandText = cubeName;
+			xlPC.MaintainConnection = true;
+			xlPC.CreatePivotTable(
+				xlRG.get_Address(miss, miss, Microsoft.Office.Interop.Excel.XlReferenceStyle.xlR1C1, miss, miss),
+				"Name1",
+				miss,
+				Microsoft.Office.Interop.Excel.XlPivotTableVersionList.xlPivotTableVersionCurrent);
+			
+			xlWB.Close(false, miss, miss);
+			m_xlApp.Quit();
+			m_xlApp = null;
+		}
+	}
+}
