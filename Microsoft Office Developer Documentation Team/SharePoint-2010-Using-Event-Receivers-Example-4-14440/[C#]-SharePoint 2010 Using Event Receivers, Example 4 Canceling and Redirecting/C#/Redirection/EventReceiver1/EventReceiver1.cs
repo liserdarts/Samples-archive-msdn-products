11861e1320514f7f092e5e93351956cb19e95653
TerryFei <v-tiafe@microsoft.com>
2018-06-11 16:22:18 +0800

    Push project SharePoint 2010: Using Event Receivers, Example 4: Canceling and Redirecting

---
+++ Microsoft Office Developer Documentation Team/SharePoint-2010-Using-Event-Receivers-Example-4-14440/[C#]-SharePoint 2010 Using Event Receivers, Example 4 Canceling and Redirecting/C#/Redirection/EventReceiver1/EventReceiver1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Security;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+
+namespace Redirection.EventReceiver1
+{
+    /// <summary>
+    /// Web Events
+    /// </summary>
+    public class EventReceiver1 : SPWebEventReceiver
+    {
+       /// <summary>
+       /// A site is being moved.
+       /// </summary>
+       public override void WebMoving(SPWebEventProperties properties)
+       {
+           properties.Status = SPEventReceiverStatus.CancelWithRedirectUrl;
+           properties.RedirectUrl = properties.Web.ServerRelativeUrl + "/_layouts/settings.aspx";
+       }
+
+       public override void WebAdding(SPWebEventProperties properties)
+       {
+           properties.Status = SPEventReceiverStatus.CancelWithRedirectUrl;
+           properties.RedirectUrl = properties.Web.ServerRelativeUrl + "/_layouts/settings.aspx";
+       }
+    }
+}
---
+++ Microsoft Office Developer Documentation Team/SharePoint-2010-Using-Event-Receivers-Example-4-14440/[C#]-SharePoint 2010 Using Event Receivers, Example 4 Canceling and Redirecting/C#/Redirection/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="a2114591-21d6-49cf-8253-f044778fd9a5" description="My Event Receiver Feature" featureId="a2114591-21d6-49cf-8253-f044778fd9a5" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="Redirection Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="f6dfedca-8972-4d73-9e65-8bafc0070cfa" />
+  </projectItems>
+</feature>
