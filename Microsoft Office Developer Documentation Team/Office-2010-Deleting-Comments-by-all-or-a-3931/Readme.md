e47d963f4a9c4f7ffb51a9fa8f09f03b2f3293e8
TerryFei <v-tiafe@microsoft.com>
2018-06-11 15:57:09 +0800

    Push project Office 2010: Deleting Comments by all or a Specific Author in Word 2010

---
+++ Microsoft Office Developer Documentation Team/Office-2010-Deleting-Comments-by-all-or-a-3931/Readme.md
@@ -0,0 +1,28 @@
+# Office 2010: Deleting Comments by all or a Specific Author in Word 2010
+## Requires
+* Visual Studio 2010
+## License
+* Apache License, Version 2.0
+## Technologies
+* Open XML SDK 2.0
+* Word 2010
+* Office 2010
+## Topics
+* deleting comments
+## IsPublished
+* True
+## ModifiedDate
+* 2011-07-28 03:06:35
+## Description
+
+<h2><strong>Introduction</strong></h2>
+<p>Learn how to use the strongly typed classes in the Open XML SDK 2.0 to delete comments by all or specific authors in a Word document, without loading the document into Microsoft Word. This sample accompanies the article
+<a href="http://msdn.microsoft.com/en-us/library/gg537288.aspx">Deleting Comments By All Authors or Specific Authors in Word 2010 Documents by Using the Open XML SDK 2.0</a> in the MSDN Library.</p>
+<h2><strong>Description</strong></h2>
+<p>The Open XML file formats allow you to delete comments in Microsoft Word documents, but doing this requires some effort. The Open XML SDK 2.0 adds strongly typed classes that grant access to the Open XML file formats. In doing so, the SDK simplifies the
+ tasks of retrieving a list of and deleting comments. This code sample shows how to the use the SDK to complete this goal. The sample includes the code that you need to delete comments by one or all authors in a Word 2007 or Word 2010 document.</p>
+<p>To use the sample, install the Open XML SDK 2.0. The sample also uses a modified version of code included as part of a set of code examples for the Open XML SDK 2.0. The accompanying article also includes a link to the full set of code examples, although
+ you can use the sample without downloading and installing the code examples.</p>
+<p>The sample application demonstrates only some the available properties and methods that are provided by the Open XML SDK 2.0 that you can interact with when modifying document structure. For more information, investigate the documentation that is included
+ with the Open XML SDK 2.0 Productivity Tool: Click the Open XML SDK Documentation tab in the lower-left corner of the application window, and search for the class that you would like to study. Although the documentation does not currently include code examples,
+ given the sample shown here and the documentation, you should be able to successfully modify the sample application.</p>
---
+++ Microsoft Office Developer Documentation Team/Office-2010-Deleting-Comments-by-all-or-a-3931/[C#]-Office 2010 Deleting Comments by all or a Specific Author in Word 2010/C#/WordDeleteComments/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+
+namespace WordDeleteComments
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      WDDeleteComments("C:\\Temp\\Comments.docx", "Ken Getz");
+    }
+
+
+    // Delete comments by a specific author. Pass an empty string for the author to delete all comments, by all authors.
+    public static void WDDeleteComments(string fileName, string author = "")
+    {
+      // Get an existing Wordprocessing document.
+      using (WordprocessingDocument document = 
+        WordprocessingDocument.Open(fileName, true))
+      {
+        // Set commentPart to the document WordprocessingCommentsPart, 
+        // if it exists.
+        WordprocessingCommentsPart commentPart =
+          document.MainDocumentPart.WordprocessingCommentsPart;
+
+        // If no WordprocessingCommentsPart exists, there can be no comments. 
+        // Stop execution and return from the method.
+        if (commentPart == null)
+        {
+          return;
+        }
+
+        List<Comment> commentsToDelete =
+          commentPart.Comments.Elements<Comment>().ToList();
+
+        // Create a list of comments by the specified author.
+        if (!String.IsNullOrEmpty(author))
+        {
+          commentsToDelete = commentsToDelete.
+            Where(c => c.Author == author).ToList();
+        }
+        IEnumerable<string> commentIds = 
+          commentsToDelete.Select(r => r.Id.Value);
+
+        // Delete each comment in commentToDelete from the 
+        // Comments collection.
+        foreach (Comment c in commentsToDelete)
+        {
+          c.Remove();
+        }
+
+        // Save comment part change.
+        commentPart.Comments.Save();
+
+        Document doc = document.MainDocumentPart.Document;
+
+        // Delete CommentRangeStart within main document.
+        List<CommentRangeStart> commentRangeStartToDelete =
+          doc.Descendants<CommentRangeStart>().
+          Where(c => commentIds.Contains(c.Id.Value)).ToList();
+        foreach (CommentRangeStart c in commentRangeStartToDelete)
+        {
+          c.Remove();
+        }
+
+        // Delete CommentRangeEnd within the main document.
+        List<CommentRangeEnd> commentRangeEndToDelete =
+          doc.Descendants<CommentRangeEnd>().
+          Where(c => commentIds.Contains(c.Id.Value)).ToList();
+        foreach (CommentRangeEnd c in commentRangeEndToDelete)
+        {
+          c.Remove();
+        }
+
+        // Delete CommentReference within main document.
+        List<CommentReference> commentRangeReferenceToDelete =
+          doc.Descendants<CommentReference>().
+          Where(c => commentIds.Contains(c.Id.Value)).ToList();
+        foreach (CommentReference c in commentRangeReferenceToDelete)
+        {
+          c.Remove();
+        }
+
+        // Save changes back to the MainDocumentPart part.
+        doc.Save();
+      }
+    }     
+
+  }
+}
