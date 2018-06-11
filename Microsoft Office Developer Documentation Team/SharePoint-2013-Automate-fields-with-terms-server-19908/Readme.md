7dd534afffe68d50ffe2b1e4d0571744959f924e
TerryFei <v-tiafe@microsoft.com>
2018-06-11 17:45:28 +0800

    Push project SharePoint 2013: Automate tagging fields with terms (server object model)

---
+++ Microsoft Office Developer Documentation Team/SharePoint-2013-Automate-fields-with-terms-server-19908/Readme.md
@@ -0,0 +1,116 @@
+# SharePoint 2013: Automate tagging fields with terms (server object model)
+## Requires
+* Visual Studio 2012
+## License
+* Apache License, Version 2.0
+## Technologies
+* SharePoint Server 2013
+## Topics
+* metadata
+* enterprise content management (ECM)
+* taxonomy
+* content management
+## IsPublished
+* True
+## ModifiedDate
+* 2013-02-06 03:45:59
+## Description
+
+<h1 id="header">Description of the sample</h1>
+<div id="mainSection">
+<div id="mainBody">
+<div class="section" id="sectionSection0">
+<p>This sample is part of a series that demonstrates how to use the managed metadata service to create taxonomy hierarchies and apply tags to documents. This code extends the functionality found in the
+<span><a href="http://code.msdn.microsoft.com/SharePoint-2013-Synchronize-4c191e68">SharePoint 2013: Synchronize term sets with the term store using the server object model</a></span> sample by introducing an export method that exports a CSV file that you can
+ edit and an import method that imports data from that file.</p>
+<h3 class="subHeading">Prerequisites</h3>
+<div class="subsection">
+<p>This sample requires the following:</p>
+<ul>
+<li>
+<p>Visual Studio 2012 or Visual Studio 2010</p>
+</li><li>
+<p>When developing using Visual Studio 2012, the SharePoint development tools in Visual Studio 2012 are required.</p>
+</li><li>
+<p>A SharePoint 2013 development environment</p>
+</li><li>
+<p>A publishing site</p>
+</li><li>
+<p>At least one single-valued managed metadata column that is bound to a term set that contains terms</p>
+</li></ul>
+</div>
+<h3 class="subHeading">Key components of the sample</h3>
+<div class="subsection">
+<p>This sample contains the following files:</p>
+<ul>
+<li>
+<p>Program.cs, which contains all of the C# code for the sample</p>
+</li><li>
+<p>ExternalTaxonomyData.xml, which contains test data that represents taxonomy data from an external source</p>
+</li></ul>
+</div>
+<h3 class="subHeading">Configure the sample</h3>
+<div class="subsection">
+<p>Follow these steps to configure the sample:</p>
+<ul>
+<li>
+<p>Open the Program.cs file and, if applicable, change the URL of the SharePoint <span>
+<span class="keyword">SPSite</span></span> object from http://localhost/ to your site URL.</p>
+</li><li>
+<p>If you want this sample to create a sample input file, uncomment the code block that generates an example XML file. This is useful if the XML schema has changed.</p>
+</li><li>
+<p>Configure a SharePoint 2013 list that contains the documents to be tagged. Create at least one single-valued managed metadata column that is bound to a term set. That term set must contain at least one term.</p>
+</li><li>
+<p>To generate the <span><span class="keyword">AutomatedTagging.csv</span></span> file, change the
+<span class="code">#if</span> directive in the <span class="code">Main</span> method to enable the
+<span class="code">ExportTemplate</span> mode.</p>
+</li><li>
+<p>Open <span><span class="keyword">AutomatedTagging.csv</span></span> in Excel and enter the name of the taxonomy term next to the URL for each document that you want to tag.</p>
+</li></ul>
+</div>
+<h3 class="subHeading">Run and test the sample</h3>
+<div class="subsection">
+<p>This sample was designed to be run locally on the computer running SharePoint 2013.</p>
+<ul>
+<li>
+<p>Press <span class="ui">F5</span> to compile and run the sample.</p>
+</li></ul>
+<p>When configured to export, the sample generates the <span><span class="keyword">AutomatedTagging.csv</span></span> file. When configured to import, the sample applies the tags from the
+<span><span class="keyword">AutomatedTagging.csv</span></span> file to the corresponding documents.</p>
+</div>
+<h3 class="subHeading">Change log</h3>
+<div class="subsection">
+<div class="caption"></div>
+<div class="tableSection">
+<table cellspacing="2" cellpadding="5" width="50%" frame="lhs">
+<tbody>
+<tr>
+<th>
+<p>Version</p>
+</th>
+<th>
+<p>Date</p>
+</th>
+</tr>
+<tr>
+<td>
+<p>First release</p>
+</td>
+<td>
+<p>December 6, 2012</p>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+</div>
+</div>
+<h1 class="heading">Related content</h1>
+<div class="section" id="seeAlsoSection">
+<div class="seeAlsoStyle"><span><a href="http://code.msdn.microsoft.com/SharePoint-2013-Synchronize-4c191e68" target="_blank">SharePoint 2013: Synchronize term sets with the term store using the server object model</a></span></div>
+<div class="seeAlsoStyle"><span><a href="http://code.msdn.microsoft.com/SharePoint-2013-Synchronize-d40638d1" target="_blank">SharePoint 2013: Synchronize term sets with the term store using the client object model</a></span></div>
+<div class="seeAlsoStyle"><span><a href="http://code.msdn.microsoft.com/SharePoint-2013-Automate-579cfc54" target="_blank">SharePoint 2013: Automate tagging fields with terms by using the client object model</a></span></div>
+<div class="seeAlsoStyle"><a href="http://msdn.microsoft.com/library/08e4e4e1-d960-43fa-85df-f3c279ed6927.aspx" target="_blank">Start: Set up a development environment for SharePoint 2013</a></div>
+</div>
+</div>
+</div>
---
+++ Microsoft Office Developer Documentation Team/SharePoint-2013-Automate-fields-with-terms-server-19908/[C#]-SharePoint 2013 Automate tagging fields with terms (server object model)/C#/SPAutomatedTagging_Server/Program.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.Office.Server.Utilities;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Taxonomy;
+
+namespace AutomatedTagging
+{
+
+    #region Helper Classes
+
+    class ExcelRow
+    {
+        public string ListItemUrl;
+
+        // InternalName, Guid
+        public List<KeyValuePair<string, Guid>> Pairs = new List<KeyValuePair<string, Guid>>();
+
+        public bool Processed = false;
+        int CsvLineNumber;
+
+        public ExcelRow(string listItemUrl, int csvLineNumber)
+        {
+            this.ListItemUrl = listItemUrl;
+            this.CsvLineNumber = csvLineNumber;
+        }
+    }
+
+    #endregion
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log("\r\nAutomatedTagging (Server OM): Connecting...\r\n");
+
+            using (SPSite site = new SPSite("http://localhost/"))
+            {
+                using (SPWeb web = site.OpenWeb())
+                {
+                    SPList list = web.Lists["Documents"];
+
+#if false
+          ExportTemplate(list);
+#else
+                    ImportTemplate(list);
+#endif
+                }
+            }
+
+            if (Debugger.IsAttached)
+                Debugger.Break();
+        }
+
+        static void ExportTemplate(SPList list)
+        {
+            Log("Writing AutomatedTagging.csv...");
+
+            Dictionary<SPContentTypeId, List<Guid>> taxonomyFieldsByContentTypeId = new Dictionary<SPContentTypeId, List<Guid>>();
+
+            using (StreamWriter streamWriter = new StreamWriter("AutomatedTagging.csv"))
+            {
+                streamWriter.WriteLine("Filename,,Field,Tags,Field,Tags,...");
+                ContentIterator contentIterator = new ContentIterator();
+                contentIterator.ProcessListItems(list,
+                    delegate(SPListItem listItem)
+                    {
+                        List<Guid> fieldGuids;
+                        if (!taxonomyFieldsByContentTypeId.TryGetValue(listItem.ContentTypeId, out fieldGuids))
+                        {
+                            fieldGuids = new List<Guid>();
+
+                            foreach (SPField field in listItem.ContentType.Fields)
+                            {
+                                // Alternatively, the code could also support taxonomy fields that can take multiple values here.
+                                if (field.TypeAsString == "TaxonomyFieldType")
+                                    fieldGuids.Add(field.Id);
+                            }
+
+                            taxonomyFieldsByContentTypeId.Add(listItem.ContentTypeId, fieldGuids);
+                        }
+
+                        if (fieldGuids.Count > 0)
+                        {
+                            streamWriter.Write(listItem.Url + ",");
+
+                            foreach (Guid fieldGuid in fieldGuids)
+                            {
+                                SPField field = listItem.Fields[fieldGuid];
+                                streamWriter.Write("," + field.InternalName + ",");
+                            }
+                            streamWriter.WriteLine();
+                        }
+                    },
+                    delegate(SPListItem listItem, Exception e)
+                    {
+                        Debug.WriteLine("Error for item " + listItem.Url
+                            + ": " + e.Message);
+                        return true;
+                    }
+                );
+
+            }
+        }
+
+        class TermSetLookup
+        {
+            Dictionary<string, Guid> TermsByName = new Dictionary<string, Guid>(StringComparer.OrdinalIgnoreCase);
+
+            public TermSetLookup(TermSet termSet)
+            {
+                foreach (Term term in termSet.GetAllTerms())
+                    TermsByName.Add(term.Name, term.Id);
+            }
+
+            public Guid GetTermId(string label)
+            {
+                return TermsByName[label];
+            }
+        }
+
+        static void ImportTemplate(SPList list)
+        {
+            TaxonomySession taxonomySession = new TaxonomySession(list.ParentWeb.Site, updateCache: true);
+
+            // STEP 1: Load the TermSet objects for the SPField objects.
+            Dictionary<string, TermSetLookup> termSetsByInternalName = new Dictionary<string, TermSetLookup>(StringComparer.OrdinalIgnoreCase);
+
+            foreach (SPField field in list.Fields)
+            {
+                // Alternatively, the code could also support taxonomy fields that can take multiple values here.
+                if (field.TypeAsString != "TaxonomyFieldType")
+                    continue;
+
+                TaxonomyField taxonomyField = field as TaxonomyField;
+                if (taxonomyField == null)
+                    continue;
+
+                TermStore termStore = taxonomySession.TermStores[taxonomyField.SspId];
+                TermSet termSet = termStore.GetTermSet(taxonomyField.TermSetId);
+                termSetsByInternalName.Add(field.InternalName, new TermSetLookup(termSet));
+            }
+
+            // STEP 2: Load the Excel file.
+            Dictionary<string, ExcelRow> excelRowsByUrl = new Dictionary<string, ExcelRow>(StringComparer.OrdinalIgnoreCase);
+
+            // Parse the input file.
+            Log("Reading AutomatedTagging.csv...");
+
+            using (StreamReader streamReader = new StreamReader("AutomatedTagging.csv"))
+            {
+                if (!streamReader.ReadLine().Contains("Filename"))
+                    throw new InvalidOperationException("Invalid file format; header is missing");
+
+                int lineNumber = 1;
+                for (; ; )
+                {
+                    string line = streamReader.ReadLine();
+                    ++lineNumber;
+                    if (line == null)
+                        break;
+
+                    string[] csvValues = ParseCsvLine(line);
+                    if (csvValues == null)
+                        throw new InvalidOperationException("[line " + lineNumber + "]: Syntax error");
+
+                    if (csvValues.Length < 1)
+                        continue;
+
+                    ExcelRow excelRow = new ExcelRow(csvValues[0], lineNumber);
+                    for (int i = 2; i + 1 < csvValues.Length; )
+                    {
+                        string key = csvValues[i++].Trim();
+                        if (key == "") break;
+
+                        string value = csvValues[i++].Trim();
+                        if (value == "") break;
+
+                        SPField field = list.Fields.GetFieldByInternalName(key);
+                        TermSetLookup termSetLookup = termSetsByInternalName[key];
+                        Guid termId = termSetLookup.GetTermId(value);
+
+                        excelRow.Pairs.Add(new KeyValuePair<string, Guid>(field.InternalName, termId));
+                    }
+
+                    excelRowsByUrl.Add(excelRow.ListItemUrl, excelRow);
+                }
+            }
+
+            // STEP 3: Update the list items.
+            ContentIterator contentIterator = new ContentIterator();
+            contentIterator.ProcessListItems(list,
+                delegate(SPListItem listItem)
+                {
+                    ExcelRow excelRow;
+                    if (!excelRowsByUrl.TryGetValue(listItem.Url, out excelRow))
+                        return;
+
+                    excelRow.Processed = true;
+
+                    bool updated = false;
+                    foreach (KeyValuePair<string, Guid> pair in excelRow.Pairs)
+                    {
+                        TaxonomyField taxonomyField = (TaxonomyField)listItem.Fields.GetFieldByInternalName(pair.Key);
+
+                        TaxonomyFieldValue taxonomyFieldValue = new TaxonomyFieldValue(taxonomyField);
+                        taxonomyFieldValue.TermGuid = pair.Value.ToString();
+
+                        TaxonomyFieldValue oldValue = listItem[taxonomyField.Id] as TaxonomyFieldValue;
+                        if (oldValue == null || oldValue.TermGuid != taxonomyFieldValue.TermGuid)
+                        {
+                            taxonomyField.SetFieldValue(listItem, taxonomyFieldValue);
+                            updated = true;
+                        }
+                    }
+
+                    if (updated)
+                    {
+                        Log("Updating item: " + listItem.Url);
+                        listItem.Update();
+                    }
+                },
+                delegate(SPListItem listItem, Exception e)
+                {
+                    Log("Error processing item " + listItem.Url
+                        + ": " + e.Message);
+                    return true;
+                }
+            );
+
+            // Were any items missed?
+            Log("");
+            List<ExcelRow> missedRows = excelRowsByUrl.Values.Where(row => !row.Processed).ToList();
+            if (missedRows.Count > 0)
+            {
+                Log("Failed to match these rows");
+                foreach (ExcelRow row in missedRows)
+                    Log("  " + row.ListItemUrl);
+            }
+
+        }
+
+        #region Utilities
+
+        static Regex csvParseRegex = new Regex(@"
+^
+(
+   (
+     (""
+        (?<value>  ( [^""] | """" )*  )
+     "") |
+     (?<value> [^"",][^,]* ) |
+     (?<value>)
+   )
+   ,
+)*
+   (
+     (""
+        (?<value>  ( [^""] | """" )*  )
+     "") |
+     (?<value> [^"",][^,]* ) |
+     (?<value>)
+   )
+$", RegexOptions.ExplicitCapture | RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
+
+        static string[] ParseCsvLine(string csvLine)
+        {
+            Match match = csvParseRegex.Match(csvLine);
+            if (match.Success)
+            {
+                return (from Capture capture in match.Groups["value"].Captures
+                        select capture.Value.Replace("\"\"", "\""))
+                    .ToArray();
+            }
+            return null;
+        }
+
+        static void Log(string message)
+        {
+            Console.WriteLine(message);
+            Debug.WriteLine(message);
+        }
+
+        #endregion
+    }
+}
+
