30df9cec7b8446d9104f41ad96f46cefadbc29d2
TerryFei <v-tiafe@microsoft.com>
2018-06-11 17:18:08 +0800

    Push project Apps for Office: Surface and display errors in an app

---
+++ Microsoft Office Developer Documentation Team/Apps-for-Office-Surface-and-display-errors-in-an-17360/Readme.md
@@ -0,0 +1,54 @@
+# Apps for Office: Surface and display errors in an app
+## Requires
+* Visual Studio 2012
+## License
+* Apache License, Version 2.0
+## Technologies
+* Word 2013
+* Excel 2013
+* Project Professional 2013
+* apps for Office
+* PowerPoint 2013
+## Topics
+* error checking
+## IsPublished
+* True
+## ModifiedDate
+* 2013-03-08 03:09:15
+## Description
+
+<p><span style="font-size:small">The code that generates the custom error and displays it to the user is contained in the CodeSample_SurfaceErrors.js file. The sample demonstrates how to define a custom error and then how to display it to a user using a dynamically
+ created div as a <em>toast notification</em>. The div appears at the bottom of the user interface for the app and includes a
+<strong>Close</strong> button to dismiss the div. The code that creates the toast notification is included in the toast.js file.</span></p>
+<h1>Prerequisites</h1>
+<p><span style="font-size:small">This sample requires the following:</span></p>
+<ul>
+<li><span style="font-size:small">Word 2013, Excel 2013, PowerPoint 2013, or Project Professional 2013.</span>
+</li><li><span style="font-size:small">Visual Studio 2012, apps for Office project templates.</span>
+</li><li><span style="font-size:small">Internet Explorer 9 or Internet Explorer 10 Preview.</span>
+</li><li><span style="font-size:small">Basic familiarity with JavaScript, CSS, and jQuery.</span>
+</li></ul>
+<h1>Key components of the sample</h1>
+<p><span style="font-size:small">The <em>Apps for Office: Surface and display errors in an app</em> sample app contains the following notable files:</span></p>
+<ul>
+<li><span style="font-size:small">The CodeSample_SurfaceErrors project, including:</span>
+<ul>
+<li><span style="font-size:small">CodeSample_SurfaceErrors.xml manifest</span> </li><li><span style="font-size:small">CodeSample_SurfaceErrors.js file</span> </li><li><span style="font-size:small">CodeSample_SurfaceErrors.html file</span> </li><li><span style="font-size:small">toast.js file</span> </li><li><span style="font-size:small">App.css file</span> </li></ul>
+</li></ul>
+<h1>Configure the sample</h1>
+<p><span style="font-size:small">No additional configuration is necessary to run the sample.</span></p>
+<h1>Build the sample</h1>
+<p><span style="font-size:small">Choose the F5 key in Visual Studio 2012 to build and deploy the app.</span></p>
+<h1>Run and test the sample</h1>
+<ol>
+<li><span style="font-size:small">Choose the F5 key to build and deploy the app.</span>
+</li><li><span style="font-size:small">Use the app&rsquo;s interface to define a custom error title and message.</span>
+</li></ol>
+<h1>Troubleshooting</h1>
+<p><span style="font-size:small">If the app fails to install, ensure that the <strong>
+SourceLocation</strong> element in the CodeSample_SurfaceErrors.xml has the correct URL value for the
+<em>DefaultValue</em> attribute.</span></p>
+<h1>Related content</h1>
+<ul>
+<li><span style="font-size:small"><a href="http://msdn.microsoft.com/library/b27e70c3-d87d-4d27-85e0-103996273298">JavaScript API for Office</a></span>
+</li></ul>
---
+++ Microsoft Office Developer Documentation Team/Apps-for-Office-Surface-and-display-errors-in-an-17360/[C#]-Apps for Office Surface and display errors in an app/C#/CodeSample_SurfaceErrors_v3Web/Scripts/CodeSample_SurfaceErrors_v3.js
@@ -0,0 +1,95 @@
+﻿// Get the values entered into the task pane.
+var errTitle, errMessage;
+
+// Add any initialization logic to this function
+Office.initialize = function (reason) {
+    $(document).ready(function () {
+
+        errTitle = document.getElementById('error-title');
+        errMessage = document.getElementById('error-message');
+
+        // Detect the availability of the HTML5 placeholder attribute.
+        // Otherwise, use the focusin/focusout events to dynamically change
+        // placeholder text.
+        if (typeof errTitle.placeholder !== 'undefined') {
+            errTitle.placeholder = "Type an error name";
+            errMessage.placeholder = "Type an error message";
+        }
+        else {
+            errTitle.value = "Type an error name";
+            errMessage.value = "Type an error message";
+
+            // Wire up event listeners to input elements.
+            errTitle.addEventListener("focusin", function (evt) {
+                changeText(evt.target, false);
+            });
+            errTitle.addEventListener("focusout", function (evt) {
+                changeText(evt.target, false);
+            });
+            errMessage.addEventListener("focusin", function (evt) {
+                changeText(evt.target, false);
+            });
+            errMessage.addEventListener("focusout", function (evt) {
+                changeText(evt.target, false);
+            });
+        }
+
+        // Tell the user that the app has initialized.
+        Toast.showToast("Event", "App initialized.")
+    });
+}
+
+// Saves the specified settings typed into the textboxes and
+// throws a custom defined error.
+function throwError() {
+    try {
+
+        // Define and throw a custom error.
+        var customError = { name: errTitle.value, message: errMessage.value }
+        throw customError;
+
+    }
+    catch (err) {
+
+        // Catch the error and display it to the user.
+        Toast.showToast("Error: " + err.name, err.message);
+    }
+    finally {
+        if (typeof errTitle.placeholder === 'undefined') {
+            // Change the text in the text boxes.
+            changeText(errTitle, true);
+            changeText(errMessage, true);
+        }
+        else {
+            errTitle.value = "";
+            errMessage.value = "";
+
+        }
+    }
+}
+
+// Changes the UI text for onfocusin, onfocusout, and save/get events.
+function changeText(node, isAction) {
+
+    // Gets the id of the node that raised the event.
+    var inputId = node.id;
+    var inputText = "Type an error ";
+
+    // Changes the text displayed in the text boxes.
+    switch (inputId) {
+        case "error-title":
+            inputText += "name";
+            break;
+        case "error-message":
+            inputText += "message";
+            break;
+    }
+
+    // Determines whether an action has just taken place, 
+    // or whether the user has started making changes to 
+    // the text boxes.
+    if (node.value == inputText) { node.value = ""; }
+    else if (node.value != "" & !isAction) { }
+    else { node.value = inputText; }
+
+}
