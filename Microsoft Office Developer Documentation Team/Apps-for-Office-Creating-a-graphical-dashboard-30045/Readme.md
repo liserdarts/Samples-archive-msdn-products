5360822010e9cb9a3fc451343429426e70329f00
TerryFei <v-tiafe@microsoft.com>
2018-06-12 08:56:02 +0800

    Push project Apps for Office: Creating a graphical dashboard control using the HTML 5 Canvas

---
+++ Microsoft Office Developer Documentation Team/Apps-for-Office-Creating-a-graphical-dashboard-30045/Readme.md
@@ -0,0 +1,137 @@
+# Apps for Office: Creating a graphical dashboard control using the HTML 5 Canvas
+## Requires
+* Visual Studio 2013
+## License
+* Apache License, Version 2.0
+## Technologies
+* Javascript
+* HTML5
+* Excel 2013
+* HTML5/JavaScript
+## Topics
+* Graphical Interaction
+* HTML5
+* developer dashboard
+* HTML 5 Canvas ID
+* HTML5/JavaScript
+## IsPublished
+* True
+## ModifiedDate
+* 2014-07-24 11:50:22
+## Description
+
+<div id="header">
+<table id="bottomTable" cellspacing="0" cellpadding="0">
+<tbody>
+<tr id="headerTableRow1">
+<td align="left"><span id="runningHeaderText">&nbsp;</span></td>
+</tr>
+<tr id="headerTableRow2">
+<td align="left"><strong><span id="nsrTitle" style="font-size:medium">Apps for Office: Creating a graphical dashboard control using the HTML 5 Canvas</span></strong></td>
+</tr>
+</tbody>
+</table>
+</div>
+<div id="mainSection">
+<div id="mainBody">
+<div class="summary">
+<p><span class="label">Summary:</span>&nbsp;&nbsp;Build a basic app for Office that displays an interactive graphical control connects to values in an Excel spreadsheet.</p>
+</div>
+<div class="introduction">
+<p><strong>In this article</strong><br>
+<a href="file://ipoawsfs201/DropZone/Rawhide/FileDropOff/NormSohl/InteractiveControl/readme.htm#O15Readme_Description">Description of the sample</a><br>
+<a href="file://ipoawsfs201/DropZone/Rawhide/FileDropOff/NormSohl/InteractiveControl/readme.htm#O15Readme_Prereq">Prerequisites</a><br>
+<a href="file://ipoawsfs201/DropZone/Rawhide/FileDropOff/NormSohl/InteractiveControl/readme.htm#O15Readme_components">Key components of the sample</a><br>
+<br>
+<a href="file://ipoawsfs201/DropZone/Rawhide/FileDropOff/NormSohl/InteractiveControl/readme.htm#O15Readme_test">Run and test the sample</a><br>
+<a href="file://ipoawsfs201/DropZone/Rawhide/FileDropOff/NormSohl/InteractiveControl/readme.htm#O15Readme_Changelog">Change log</a><br>
+<a href="file://ipoawsfs201/DropZone/Rawhide/FileDropOff/NormSohl/InteractiveControl/readme.htm#O15Readme_RelatedContent">Related content</a></p>
+<p>&nbsp;</p>
+</div>
+<a name="O15Readme_Description">
+<h2 class="heading">Description of the sample</h2>
+<div class="section" id="sectionSection0">
+<p>This sample shows how to create an interactive graphical control that will display values from an Excel spreadsheet. The control can also be used to change the value using a mouse or touch. Such controls can be used to make a live dashboard that displays
+ business status.</p>
+</div>
+</a><a name="O15Readme_Prereq">
+<h2 class="heading">Prerequisites</h2>
+<div class="section" id="sectionSection1">
+<p>This sample requires the following:</p>
+<ul>
+<li>
+<p>A Windows 8 development workstation.</p>
+</li><li>
+<p>Office 365.</p>
+</li><li>
+<p>Visual Studio 2013.</p>
+</li><li>Microsoft Office Developer Tools for Visual Studio 2013 - May 2014 Update </li></ul>
+</div>
+</a><a name="O15Readme_components">
+<h2 class="heading">Key components of the sample</h2>
+<div class="section" id="sectionSection2">
+<p>The sample zip file contains the following:</p>
+<ul>
+<li>
+<p>A Visual Studio 2012 solution file that contains the AppManifest.xml file and source for the app named InteractiveControl.</p>
+</li><li>
+<p>A copy of this readme.</p>
+</li></ul>
+</div>
+</a><a name="O15Readme_config">
+<h2 class="heading">Configure the sample</h2>
+<div class="section" id="sectionSection3">
+<p>Follow these steps to configure the sample.</p>
+<ol>
+<li>
+<p>On a Windows 8 workstation, open Visual Studio 2013.</p>
+</li><li>
+<p>On the <span class="ui">File</span> menu, click <span class="ui">Open</span>,
+<span class="ui">Project/Solution</span> and then navigate to the folder where you unzipped the project. Select the InteractiveControl.sln file and click
+<span class="ui">Open</span>.</p>
+</li></ol>
+</div>
+</a><a name="O15Readme_test">
+<h2 class="heading">Run and test the sample</h2>
+<div class="section" id="sectionSection4">
+<p>Run the solution in debug mode to explore the code sample. Since the app is web based there is no need to build it.</p>
+</div>
+</a><a name="O15Readme_Changelog">
+<h2 class="heading">Change log</h2>
+<div class="section" id="sectionSection5">
+<div class="caption"></div>
+<div class="tableSection">
+<table cellspacing="2" cellpadding="5" width="50%" frame="lhs">
+<tbody>
+<tr>
+<th>
+<p>Version</p>
+</th>
+<th>
+<p>Date</p>
+</th>
+</tr>
+<tr>
+<td>
+<p>First version</p>
+</td>
+<td>
+<p>May, 14 2014</p>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+</div>
+</a><a name="O15Readme_RelatedContent">
+<h2 class="heading">Related content</h2>
+</a>
+<div class="section" id="sectionSection6"><a name="O15Readme_RelatedContent"></a>
+<ul>
+<a name="O15Readme_RelatedContent"></a>
+<li><a name="O15Readme_RelatedContent"></a><a href="http://msdn.microsoft.com/EN-US/library/office/jj220077(v=office.15).aspx" target="_blank">Develop apps for Office</a>
+<p>&nbsp;</p>
+</li></ul>
+</div>
+</div>
+</div>
---
+++ Microsoft Office Developer Documentation Team/Apps-for-Office-Creating-a-graphical-dashboard-30045/[C#]-Apps for Office Creating a graphical dashboard control using the HTML 5 Canvas/C#/InteractiveControlWeb/App/Home/Home.js
@@ -0,0 +1,194 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+        });
+    };
+
+    $(document).ready(function () {
+        // get element
+        var canvas = document.getElementById('control');
+        canvas.onpointermove = TouchHitTest;
+        canvas.onpointerdown = TouchHitTest;
+        canvas.onpointerup = TouchHitTest;
+        window.onload = initSelectionHandler;
+        drawControl(.4);
+    });
+
+    function initSelectionHandler(){
+        Office.context.document.addHandlerAsync(Office.EventType.DocumentSelectionChanged, SelectionChanged);
+    }
+
+    function SelectionChanged(e) {
+        var dataString = "uninitialized";
+
+        // get data at selection
+        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text, 
+            { valueFormat: "unformatted", filterType: "all" },
+            function (asyncResult) {
+                var error = asyncResult.error;
+                if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+                    dataString = error.name + ": " + error.message;
+                } 
+                else {
+                    // Get selected data.
+                    dataString = asyncResult.value;
+
+                    // if there is no data in the cell update the value from the guage
+                    if (dataString == "") {
+                        // update the data in the cell with the data from the indicator
+                        Office.context.document.setSelectedDataAsync(intIndicator);
+                    }
+                    else {
+                        //update the guage with the value in the cell
+                        drawGauge((100-dataString)/100);
+                    }
+                }            
+            });
+    }
+
+    var intIndicator;
+
+    function drawControl(indicatorValue) {
+        if (indicatorValue > 1) indicatorValue = 1;
+        if (indicatorValue < 0) indicatorValue = 0;
+
+        intIndicator = parseInt(100 - (100 * indicatorValue));
+
+        // get element
+        var canvas = document.getElementById('control');
+
+        // get context
+        var context = canvas.getContext('2d');
+
+        // set the color to draw with
+        context.strokeStyle = "rgb(0, 162, 232)";
+        context.fillStyle = "rgb(0, 162, 232)";
+        context.lineWidth = "20";
+        context.lineCap = "round";
+        context.font = "20px tahoma";
+
+        // (re)set the identity matrix.
+        context.setTransform(1, 0, 0, 1, 0, 0);
+
+        // erase the canvas each draw cycle
+        context.clearRect(0,0,200,200);
+
+        // draw the numerical value to the canvas
+        var textCenter = context.measureText(intIndicator).width/2;
+        //context.fillText(intIndicator.toString(10), (canvas.width / 2) - (textWidth/2), canvas.height / 2);
+        context.fillText(intIndicator.toString(10), 100 - textCenter, 165);
+
+        //the arc function takes a bool that determines the direction that the arc is drawn in.  
+        //It may be easier to create descriptivly named values like these for this bool.
+        var clockwise = false;   // this is the direction that the degrees are counted in
+        var anticlockwise = true;
+
+        //we want to center the arc in the canvas.
+        var cx = canvas.clientWidth /2;
+        var cy = canvas.clientHeight /2;
+
+        // A gap of 60 gives a 300 degree gauge
+        // A gap of 90 gives a 270 degree gauge
+        // A gap of 120 gives a 240 degree gauge
+        // A gap of 180 gives a 180 degree gauge
+        // A gap of 240 gives a 120 degree gauge
+        var gap = 80;
+
+        // draw a dot at the center of the gauge
+        var radius = 5;
+        context.beginPath();
+        context.arc(cx, cy, radius, ConvertDegrees(0), ConvertDegrees(360), clockwise);
+        context.fill();
+        context.stroke();
+        //Rotate the context so that the arc function has it's origion at 3:00.  This requires rotating by 90 degrees to put it at 6:00.
+        //for more details see http://msdn.microsoft.com/en-us/library/ie/ff975430(v=vs.85).aspx 
+        context.setTransform(0, 1, 1, 0, 0, 0);  // rotate by 90 degrees
+
+        // you could also use rotate instead of setTransform:
+        //context.translate(cx, cy);          //set the center of rotation
+        //context.rotate(ConvertDegrees(90)); //rotate
+        //context.translate(-cx, -cy);        //reset
+
+        //draw the gauge background
+        context.beginPath();
+
+        // draw the circle.
+        // make radius a little less than half the width of the canvas
+        var radius = cx * .75;
+        // call the arc function
+        context.arc(cx, cy, radius, ConvertDegrees(0 + gap / 2), ConvertDegrees(360 - gap / 2), clockwise);
+        context.stroke();
+
+        // draw the tick marks
+        context.lineCap = "round";
+        context.lineWidth = "5";
+        context.strokeStyle = "white";
+
+        //Number of ticks
+        var ticks = 10;
+        var tickThickness = .1; // degrees
+        var spacing = (360 - gap) / ticks;
+        for (var loop = 0; loop <= ticks; loop++) {
+            var ticDegree = gap / 2 + spacing * loop;
+            context.beginPath();
+            context.arc(cx, cy, radius, ConvertDegrees(ticDegree - tickThickness / 2), ConvertDegrees(ticDegree + tickThickness / 2, clockwise));
+            context.stroke();
+        }
+
+        // draw the indicator
+        context.strokeStyle = "white";
+        context.lineWidth = "10";
+        context.beginPath();
+        context.arc(cx, cy, radius, ConvertDegrees((0 + gap / 2) + (360 - gap) * indicatorValue), ConvertDegrees(360 - gap / 2), clockwise);
+        context.stroke();
+    }
+
+    // arc function requires radians, Most people think in terms of degrees.
+    function ConvertDegrees(Degrees) {
+        return Degrees * Math.PI / 180;
+    }
+
+    var PointerDown = false;
+    function TouchHitTest(e) {
+
+        // stop default behavior/propagation of event to avoid selecting other elemements when mouse exits canvas and button is down.
+        e.preventDefault();
+        e.stopImmediatePropagation();
+
+        var canvas = document.getElementById('control');
+        canvas.setPointerCapture(e.pointerId); 
+        if (e.type == "pointerdown")
+            PointerDown = true;
+        else if (e.type == "pointerup")
+            PointerDown = false;
+
+        if (PointerDown == true) {
+            updateValues(e);
+        }
+    }
+
+    function ConvertWindowToCanvasCoords(e) {
+        var canvas = document.getElementById('control');
+        var point = document.getElementById('pointer');
+        var rect = canvas.getBoundingClientRect();
+        var x = e.x - rect.left;
+        var y = e.y - rect.top;
+    
+        //var indicator = 1 - (x / canvas.clientWidth);
+        return 1 - (x / canvas.clientWidth);
+    }
+
+    function updateValues(e) {
+            var indicator = ConvertWindowToCanvasCoords(e);
+            drawControl(indicator);
+
+            //intIndicator is a member of the parent class
+            Office.context.document.setSelectedDataAsync(intIndicator);
+    }
+})();
